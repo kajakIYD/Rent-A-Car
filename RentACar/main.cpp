08af625e13606953a4df2d952420cf250456a3e6
kayakIYD <ficupaolo@gmail.com>
2019-06-09 10:16:01 +0200

    Fix unhandled exception memory access

--- RentACar/main.cpp
+++ RentACar/main.cpp
@@ -24,9 +24,9 @@ void printMenu()
 }
 
 std::vector<User> userVector;
-User* activeUser = nullptr; //wskaznik na typ user, wskaznik sie nazywa activeUser, poki co pod tym wskaznikiem nie ma aktywnego uzytkownika dlatego nullptr
+//User* activeUser = nullptr; //wskaznik na typ user, wskaznik sie nazywa activeUser, poki co pod tym wskaznikiem nie ma aktywnego uzytkownika dlatego nullptr
 
-bool logIn()
+bool logIn(User& activeUser) //User& to znaczy ze przekazujemy przez referencje 
 {
 	std::cout << "Hello!\n";
 	
@@ -41,12 +41,12 @@ bool logIn()
 
 	for each (User user in userVector)
 	{
-		if (login == user.login && password == user.password)
+		if (login.compare(user.login) == 0 && password.compare(user.password) == 0)
 		{
 			std::cout << "YOU HAVE SUCCESSFULLY LOGGED IN!\n";
 			std::cout << "Welcome " << user.name;
-			activeUser = &user; //Pod wskaznikiem mamy dostep do aktywnego uzytkownika, ale dlatego ze to wskanik to musi byæ &user zamiast user
-								//& wy³uskuje adres obiektu
+			activeUser = user; //Pod wskaznikiem mamy dostep do aktywnego uzytkownika, ale dlatego ze to wskanik to musi byæ &user zamiast user
+								//& wy³uskuje adres obiektu (to jest opis dla tego co robiliœmy wskaŸnikiem). Ale akurat tutaj robimy przez referennjê
 			return true;
 		}
 	}
@@ -54,7 +54,7 @@ bool logIn()
 	return false;
 }
 
-void shopping()
+void shopping(User* activeUser)
 {
 	//Operations::showAllCars(); //tu nie odwo³ujemy sie do obiektu klasy
 
@@ -75,7 +75,7 @@ void shopping()
 		}
 		else
 		{
-			operation.rentACar(choice, *activeUser);	//tu przy activeUser musi byæ *, bo * wy³uskuje wartoœæ któr¹ pokazuje wskaŸnik
+			operation.rentACar(choice, *activeUser);	//tu przy activeUser musi byæ *, bo * wy³uskuje wartoœæ któr¹ pokazuje wskaŸnik (jeœli robimy przez wskaŸnik)
 			
 			while (choice != "e" && choice != "E" && choice != "b" && choice != "B")
 			{
@@ -86,7 +86,7 @@ void shopping()
 	}
 }
 
-void logOut()
+void logOut(User* activeUser)
 {
 	activeUser = nullptr; //wylogowanie, dlatego aktywwny uzytkownik to znowuu nullptr (nullptr to w zasadzie to samo co null w c)
 }
@@ -120,7 +120,7 @@ void signUp()
 		std::cin >> email;
 		for each (User user in userVector)
 		{
-			if (user.emailAddress == email)
+			if (user.emailAddress.compare(email) == 0)
 			{
 				std::cout << "Email exists in database!! Type another one\n";
 				emailFoundInUserVector = true;
@@ -128,7 +128,7 @@ void signUp()
 		}
 
 	} while (emailFoundInUserVector == true);
-	
+
 	std::cout << "Phone number: ";
 	std::cin >> phoneNumber;
 
@@ -140,7 +140,7 @@ void signUp()
 		std::cin >> login;
 		for each (User user in userVector)
 		{
-			if (user.login == login)
+			if (user.login.compare(login) == 0) //string::compare zwraca 0 jesli stringi sa takie same, a >0 jesli nie
 			{
 				std::cout << "Login exists in database!! Type another one\n";
 				loginFoundInUserVector = true;
@@ -153,15 +153,17 @@ void signUp()
 	std::cout << "Amount of cash: ";
 	std::cin >> amountOfCash;
 
-	userVector.push_back( User(name, surname, age, email, phoneNumber, login, password, 0, amountOfCash) );
+	userVector.push_back(User(name, surname, age, email, phoneNumber, login, password, 0, amountOfCash));
 }
 
 
 int main(void)
 {
 	User user1 = User("a", "b", 23, "a@a.pl", "333-333-333", "l", "p", 0, 10000000);
-	
+
 	userVector.push_back(user1); //dodanie user1 na koniec wektora (dynamicze przydzielanie pamiêci)
+
+	User activeUser = User(); //konstrutor domyslny, bo user jest "pusty"
 
 	std::cout << "Hello would You like to rent a car?\n";
 	
@@ -176,15 +178,15 @@ int main(void)
 
 		if (c == "1")
 		{
-			if (logIn() == true)
+			if (logIn(activeUser) == true) //login(activeUser) -> to jest przekazanie przez referencjê (mechanizm taki  jak przez wskaznik, tylko prostszy ww obs³udze i debugowaniu)
 			{
-				shopping();
+				shopping(&activeUser);
 				break;
 			}			
 		}
 		else if (c == "2")
 		{
-			logOut();
+			logOut(&activeUser);
 		}
 		else if (c == "3")
 		{
